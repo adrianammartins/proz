5396bd3b8daec00b6e0c73a27e5a253d4e3983e7
adrianammartins <61126130+adrianammartins@users.noreply.github.com>
2023-07-28 21:13:10 -0300

    Create andar.py

---
+++ andar.py
@@ -0,0 +1,13 @@
+print("código usando for")
+for andar in range(20, 0, -1): 
+  if andar != 13:
+    print(andar)
+ 
+      
+print("Código usando while")  
+andar = 20
+
+while andar > 0:
+  if andar != 13:
+    print(andar)
+  andar = andar - 1
