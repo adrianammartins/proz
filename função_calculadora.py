dc39bfb88b41d0aeba0b0602929e7b8f397e8c99
Adriana Martins <drica317@gmail.com>
2023-08-13 18:47:19 -0300

    modificacao no parametro da funcao

--- função_calculadora.py
+++ função_calculadora.py
@@ -1,4 +1,4 @@
-def calculadora(numero1, numero2, operacao):
+def calculadora(numero1, numero2,opcao):
     
     
     if opcao == 0:
@@ -20,7 +20,7 @@ while True:
     opcao= int(input("Digite o numero da opção3: (1: soma, 2: subtracao, 3:multiplicação, 4: divisão, 0:sair): "))
     
     numero1= float(input("Digite o primeiro número: "))
-    numero2= float(input("Digite o primeiro número: "))
+    numero2= float(input("Digite o segundo número: "))
     
-    resultado = calculadora(numero1, numero2, operacao)
+    resultado = calculadora(numero1, numero2, opcao)
     print("Resultado: ", resultado)
